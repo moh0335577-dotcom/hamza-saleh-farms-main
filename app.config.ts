a1e8c995304b9626b8ae4c42289035d571c62a47
ABD <abd33ismail@gmail.com>
2026-08-27 21:15:36 +0300

    Fix Netlify asset paths

--- app.config.ts
+++ app.config.ts
@@ -83,7 +83,6 @@ const config: ExpoConfig = {
     bundler: "metro",
     output: "static",
     favicon: "./assets/images/favicon.png",
-    publicPath: "/hamza-saleh-farms-main/",
   },
   plugins: [
     "expo-router",
